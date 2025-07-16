c64fc3f85d6f6388d48492392655d26cbcf1be07
JavaDevKKT <javadevkkt@gmail.com>
2025-07-16 10:20:12 +0530

    add entity and dto

--- src/main/java/com/ktech/dto/UserDTO.java
+++ src/main/java/com/ktech/dto/UserDTO.java
@@ -1,5 +1,17 @@
 package com.ktech.dto;
 
-public class UserDTO {
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // leran this
+public class UserDTO {
+	private Long id;
+	private String email;
+	private String name;
+	private String phoneNumber;
+	private String role;
+	private List<BookingDTO> bookings = new ArrayList<>();
 }
